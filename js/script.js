d47985fee89b46c5ccdeed2006cd86879bf7aa31
Adrian H <adrianhoff@gmail.com>
2024-03-13 14:23:09 +0100

    addet blur bg, cursor effekt
    Forttsat fungerer dårlig på firfox

--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
+// Sjekker om det er firfox eller chrome:
+let isFirefox = typeof InstallTrigger !== 'undefined';
+let isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+if (isChrome) {
+    // Apply CSS filter for Chrome
+    document.documentElement.style.setProperty('--my-filter', 'url(#goo) blur(40px)');
+} else if (isFirefox) {
+    // Remove or change CSS filter for Firefox
+    document.documentElement.style.setProperty('--my-filter', 'none');
+}
+
+
+// Mouse Animation:
+const cursor = document.querySelector(".interactive")
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+   // Calculate the center of the screen
+   let centerX = window.innerWidth / 2;
+   let centerY = window.innerHeight / 2;
+ 
+   cursor.style.top = (y - centerY) + "px";
+   cursor.style.left = (x - centerX) + "px";
+});
+// Mouse Cursor:
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+window.addEventListener("mousemove", function (e){
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  //Om du vil ha fixed cursor uten animation
+  //cursorOutline.style.left = `${posX}px`;
+  //cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, { duration: 100, fill: "forwards"});
+})
+
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
@@ -41,9 +88,9 @@ function darken2() {
   var img = document.getElementById("main-img");
   img.src="/image/minecraft.png";
 } 
-// loader
+// Loader
 $(window).on("load",function(){
   setTimeout(function(){ 
     $(".loader-wrapper").fadeOut("slow");
-  }, 2000); // Setter forsinkelsestid til 5000 millisekunder (5 sekunder)
+  }, 2000);
 });
